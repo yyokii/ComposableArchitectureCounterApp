feca8eed443e1a8f36aaa161737df05601755242
yyokii <yyokii.h@gmail.com>
2021-05-21 01:20:21 +0900

    Initial Commit

---
+++ ComposableArchitectureCounterApp/ComposableArchitectureCounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureCounterAppApp.swift
+//  ComposableArchitectureCounterApp
+//
+//  Created by Higashihara Yoki on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureCounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
